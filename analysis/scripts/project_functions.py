8dd2ac13d9ea49116c15f1e5417b410fb198f36f
Carl-Joshua <karel.joshar@gmail.com>
2021-07-25 00:46:10 -0700

    edit analysis/karel

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,50 +1,39 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-def load_and_process(url_or_path_to_csv_file):
-  """
-  Load and process the given data.
-  rename column headings that are necessary and insert columns desired.
+def load_and_process(path_2021, path_2020):
+    """
+    Load and process the given data.
+    rename column headings that are necessary and insert columns desired.
 
-  Parameters
-  ----------
-  url_or_path_to_csv_file : str
-      The path to the file.csv.
-      
-  Returns
-  -------
-  <class 'pandas.core.frame.DataFrame'>
+    Parameters
+    ----------
+    path_2021 : str
+      The path to world-happiness-report-2021.csv
+    path_2020 : str
+      The path to world-happiness-report-2020.csv
+
+    Returns
+    -------
+    <class 'pandas.core.frame.DataFrame'>
       The dataFrame containing the cleaned data. 
-      
-  Examples
-  --------
-  >>> make_palindrome('../../data/raw/world-happiness-report-2021.csv')
-  <class 'pandas.core.frame.DataFrame'>
-  """
+
+    Examples
+    --------
+    >>> load_and_process(path_2021='../../data/raw/world-happiness-report-2021.csv', path_2020='../../data/raw/world-happiness-report-2020.csv')
+    <class 'pandas.core.frame.DataFrame'>
+    """
+    # read supporting file
+    data_2021 = pd.read_csv(path_2021)
+    data_2020 = pd.read_csv(path_2020)
     
-  # supporting file
-  data_2020 = pd.read_csv('../../data/raw/world-happiness-report-2020.csv')
-
-  # Method Chain 1 (Load data and deal with missing data)
-
-  # no missing values and the head are self-explanatory except "ladder score"
-  data_2021 = (
-      pd.read_csv(url_or_path_to_csv_file)
-      .rename(columns={'Ladder score': 'Ladder score 2021'})
+    # no need for cleaning since data has no missing value and sorted decendingly by ladder score
+    # method chain for data wrangling
+    df = (
+        data_2021.assign(Ladder_score_2020 = data_2020['Ladder score'],
+                         Score_difference = data_2021['Ladder score'] - data_2020['Ladder score'])
+        .rename(columns={'Ladder score': 'Ladder score 2021',
+                         'Ladder_score_2020': 'Ladder score 2020',
+                         'Score_difference': 'Score difference'})
     )
-  print(type(data_2021))
-  data_2020 = (
-      pd.read_csv('../../data/raw/world-happiness-report-2020.csv')
-      .rename(columns={'Ladder score': 'Ladder score 2020'})
-  )
-
-  # Method Chain 2 (Create new columns, drop others, and do processing)
-
-  # add column 'Ladder score 2020' and 'Score difference' using pd.dataFrame.insert()
-  data_2021.insert(3, 'Ladder score 2020', data_2020['Ladder score 2020'])
-  data_2021.insert(4, 'Score difference', data_2021['Ladder score 2021'] - data_2020['Ladder score 2020'])
-
-  # Make sure to return the latest dataframe
-
-  return data_2021
+    
+    return df
