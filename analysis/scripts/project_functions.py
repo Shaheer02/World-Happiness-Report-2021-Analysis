cd4792987d521c0c62c612537296a3092ad0b785
Kaitlyn Peverley <kpev5388@gmail.com>
2021-07-27 14:54:23 -0700

    project function adding1

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -48,3 +48,35 @@ def load_and_process(url_or_path_to_csv_file):
   # Make sure to return the latest dataframe
 
   return data_2021
+
+---
+
+    # Method Chain 1
+    # No Missing Data 
+    happiness2020 = pd.read_csv("/Users/kaitlynpeverley/Desktop/COSC301/Labs/project-group10-project/data/raw/world-happiness2020.csv")
+    happiness2021 = pd.read_csv("/Users/kaitlynpeverley/Desktop/COSC301/Labs/project-group10-project/data/raw/world-happiness-report-2021.csv")
+
+
+    # Method Chain 2 
+    happinessdata1 = (happiness2021
+                     .rename(columns={'Country name': 'Country Name', 
+                                      'Regional indicator': 'Regional Indicator', 
+                                      'Ladder score': 'Ladder Score', 
+                                      'Standard error of ladder score': 'Standard Error of Ladder Score', 
+                                      'upperwhisker': 'Upper Whisker', 
+                                      'lowerwhisker': 'Lower Whisker', 
+                                      'Logged GDP per capita': 'Logged GDP Per Capita', 
+                                      'Social support': 'Social Support',
+                                      'Healthy life expectancy': 'Healthy Life Expectancy',
+                                      'Freedom to make life choices': 'Freedom to Make Life Choices',
+                                      'Perceptions of corruption': 'Perceptions of Corruption',
+                                      'Ladder score in Dystopia': 'Ladder Score in Dystopia',
+                                      'Explained by: Log GDP per capita': 'Explained by: Log GDP per Capita',
+                                      'Explained by: Perceptions of corruption': 'Explained by: Perceptions of Corruption',
+                                      'Explained by: Social support': 'Explained by: Social Support',
+                                      'Explained by: Healthy life expectancy': 'Explained by: Healthy Life Expectancy',
+                                      'Explained by: Freedom to make life choices': 'Explained by: Freedom to make Life Choices',
+                                      'Dystopia + residual': 'Dystopia and Residual',
+                                      'Ladder score': 'Ladder score 2021'})
+                   .insert(3, 'Ladder Score Percent Change From Previous Year', ((happiness2021['Ladder score'] - happiness2020['Ladder  score'])/happiness2020['Ladder score'])*100))
+    return happinessdata1
